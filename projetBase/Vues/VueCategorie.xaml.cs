1d47312f676a5ca0aed4b49969743b831917925c
thierrysio <thierry28220@icloud.com>
2025-02-25 11:06:26 +0100

    ok

--- projetBase/Vues/VueCategorie.xaml.cs
+++ projetBase/Vues/VueCategorie.xaml.cs
@@ -1,14 +1,16 @@
 using projetBase.Modeles;
 using projetBase.Services;
+using System.Collections.ObjectModel;
+
 
 namespace projetBase.Vues;
 
 public partial class VueCategorie : ContentPage
 {
     private readonly Apis _apiServices = new Apis();
-
+    public ObservableCollection<CategorieParent> CP { get; set; } = new ObservableCollection<CategorieParent>();
     public VueCategorie()
-	{
+    {
 		InitializeComponent();
 		BindingContext = this;
 
@@ -17,8 +19,18 @@ public partial class VueCategorie : ContentPage
 
     private async Task GetCategories()
     {
-       
+
+        // Appelle l’API
         var result = await _apiServices.GetAllAsync<CategorieParent>("api/mobile/categories");
-        
+
+        // On vide éventuellement la liste puis on l’alimente
+        CP.Clear();
+        if (result != null)
+        {
+            foreach (var item in result)
+            {
+                CP.Add(item);
+            }
+        }
     }
 }
