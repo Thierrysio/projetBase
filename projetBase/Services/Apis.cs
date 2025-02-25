3d8522eba2c47541dd8e2e69679e0282fa52b2a4
thierrysio <thierry28220@icloud.com>
2025-02-25 08:37:15 +0100

    ok

--- projetBase/Services/Apis.cs
+++ projetBase/Services/Apis.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,21 @@ namespace projetBase.Services
     public class Apis
     {
         public readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<ObservableCollection<T>> GetAllAsync<T>(string url)
+        {
+            try
+            {
+                var json = await _httpClient.GetStringAsync(Constantes.BaseApiAddress + url);
+                var result = JsonConvert.DeserializeObject<List<T>>(json, new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd HH:mm:ss" });
+                return new ObservableCollection<T>(result);
+            }
+            catch (Exception ex)
+            {
+                // Log or handle the exception as needed.
+                throw;
+            }
+        }
 
     }
 }
